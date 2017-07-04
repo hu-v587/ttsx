2f3a38e7d8f76b9fb2ce6d5a7cde02232301a237
hu <728198489@qq.com>
2017-07-04 19:26:41 +0800

    恢复现场

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('booktest.urls')),
-    url(r'^user/', include('df_user.urls')),
+    # url(r'^', include('booktest.urls')),
+    # url(r'^user/', include('df_user.urls')),
 ]
